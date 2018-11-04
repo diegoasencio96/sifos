fb93a827c6c664a9de146d585add6c746e2e08ef
Clare <clarero1611@gmail.com>
2018-11-04 15:41:55 -0500

    Agrega login a app usuario

--- Entregable 2/Web/SIFO/urls.py
+++ Entregable 2/Web/SIFO/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from SIFO.views import login
+from apps.usuario.views import login
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
