171fdd4e6507190c3a6f769e307ad25bf146527b
Clare <clarero1611@gmail.com>
2018-11-04 10:25:24 -0500

    menor: Agrega configuración de estaticos al proyecto

--- Entregable 2/Web/SIFO/settings.py
+++ Entregable 2/Web/SIFO/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zpgc^$-a#w*s7s@thx@+m*net286=3%wy^1ce)2x0v+t+9v37('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -122,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
