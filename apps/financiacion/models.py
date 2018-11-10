920c70c714e666c167d07c18e7fc0862cbd7090a
oabaquero <obaquerog@gmail.com>
2018-11-10 12:10:55 -0500

    Agrega migraciones

--- apps/financiacion/models.py
+++ apps/financiacion/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.utils.timezone import now
 from apps.usuario.models import Donador
+from decimal import Decimal
+
 
 # Create your models here.
 class Donacion(models.Model):
-    valor = models.CharField(max_length=50, blank=True, null=True)
+    valor = models.DecimalField(max_digits=30, decimal_places=0, default=Decimal(0))
     fecha = models.DateTimeField(default=now, blank=True)
     donador = models.ForeignKey(Donador, blank=False, null=False)
 
