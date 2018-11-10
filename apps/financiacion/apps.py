bbc1fd1565a8da5a1611863970b07c6ed4f6ec77
carlos-san <cm.090691@gmail.com>
2018-11-10 10:35:58 -0500

    Agrega registro de modelos a administrador django

--- apps/financiacion/apps.py
+++ apps/financiacion/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class DonacionConfig(AppConfig):
-    name = 'financiacion'
+    name = 'apps.financiacion'
--- apps/terreno/apps.py
+++ apps/terreno/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class PoligonoConfig(AppConfig):
-    name = 'terreno'
+    name = 'apps.terreno'
