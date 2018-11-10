f7086ea8820b7a065cd661d4a1443cf9c0d8d7ce
camilo-cajamarca <jkmilo2703@gmail.com>
2018-11-10 10:48:28 -0500

    ajustes para conexión a BD RDS AWS

--- SIFO/settings.py
+++ SIFO/settings.py
@@ -76,8 +76,18 @@ WSGI_APPLICATION = 'SIFO.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-if 'TRAVIS' in os.environ:
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+elif 'TRAVIS' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE':   'django.db.backends.postgresql_psycopg2',
