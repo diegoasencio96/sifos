f5d072a3f22a097e2ac6699a07b3c15749dde5e1
Clare <clarero1611@gmail.com>
2018-11-16 20:55:55 -0500

    Actualiza urls a Django 2.1

--- SIFO/urls.py
+++ SIFO/urls.py
@@ -5,22 +5,23 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  url('$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url('$', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  url('blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.urls import path
 from django.contrib import admin
 from apps.usuario.views import LoginView, LogoutView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$',  view=LoginView.as_view(),name="user_login"),
-    url(r'^logout/$',  view=LogoutView.as_view(),name="panel-logout"),
-    url(r'^terreno/', include('apps.terreno.urls'), name="terreno"),
-    url(r'^usuario/', include('apps.usuario.urls')),
+    path('admin/', admin.site.urls),
+    path('',  view=LoginView.as_view(),name="user_login"),
+    path('logout/',  view=LogoutView.as_view(),name="panel-logout"),
+    path('terreno/', include('apps.terreno.urls'), name="terreno"),
+    path('usuario/', include('apps.usuario.urls')),
 ]
